4b992188e6e775b72cba97ae2403d23219502798
NaomiWaterlow <naomi.waterlow1@lshtm.ac.uk>
2024-07-10 11:56:55 +0100

    Added lrt

--- regresssions.R
+++ regresssions.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(lmtest)
 
 dummy_data <- data.table(
   resistance = c(0,1,0,1,1,1), 
@@ -13,14 +14,17 @@ dummy_data
 
 # Model 1
 # assumes a global effect of sex, not for it to vary by country (intercept varies by country)
-model <- glm(resistance~sex+age_group+bug+drug+country, data = dummy_data, 
+model1 <- glm(resistance~sex+age_group+bug+drug+country, data = dummy_data, 
              family = "binomial")
 
 # Model 2
 # allows effect of sex to vary by country (i.e. intercept and slope)
-model <- glm(resistance~sex+age_group+bug+drug+country+(sex*country), data = dummy_data, 
+model2 <- glm(resistance~sex+age_group+bug+drug+country+(sex*country), data = dummy_data, 
              family = "binomial")
 
+# Is the model with interaction better than the one without
+# I.e. is there stat evidence for interaction
+lrtest(model1,model2)
 
 
 
